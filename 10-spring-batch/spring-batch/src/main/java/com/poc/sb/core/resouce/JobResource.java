b7a6c45b2a73ed721230cb2b75b89ab6370e4eee
pedroPolonea <phpolonea@hotmail.com>
2023-06-05 22:22:08 -0300

    2 - Ajustando

--- 10-spring-batch/spring-batch/src/main/java/com/poc/sb/core/resouce/JobResource.java
+++ 10-spring-batch/spring-batch/src/main/java/com/poc/sb/core/resouce/JobResource.java
@@ -1,8 +1,6 @@
 package com.poc.sb.core.resouce;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.launch.JobLauncher;
